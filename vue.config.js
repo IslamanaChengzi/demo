d61ef8ada1690cc389dbf87f6cd203af0aad47a0
luanyecheng <18341716543@139.com>
2020-06-01 23:50:47 +0800

    配置环境变量，对应进行打包

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// process.env.VUE_APP_VERSION = require('./package.json').version
+
+console.log( process.env.NODE_ENV );
+console.log( process.env.BABEL_ENV );
+
+module.exports = {
+  // config
+  
+}
