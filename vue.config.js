888a4b9e3776391abc223c94ad935543022e5a80
luanyecheng <18341716543@139.com>
2020-07-19 23:22:12 +0800

    github部署相关设置

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ console.log( process.env.NODE_ENV );
 console.log( process.env.BABEL_ENV );
 
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'? '/production-sub-path/': '/',
 	chainWebpack: config => {
 		config.module
 			.rule('pug')
